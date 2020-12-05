0e0b239d2b815d82525f7c30d5849e3a5528d7c1
melonbun <gu.karlan@gmail.com>
2020-12-05 18:29:44 +1100

    method for counting up

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mCount=0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -20,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        mCount++;
+        //make sure the view is not null
+        if(mShowCount!=null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
